76dfb0cd1bdc97e02b531c042a0c95a62ba58e49
nsantiago <nsantiago@yondu.com>
2019-04-16 16:20:51 +0800

    change channel_id to channel

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -9,8 +9,6 @@ class EventController < ApplicationController
     render json: { status: ok }, status: 200
   end
 
-  private
-
   def url_challenge
     if challenge_data['challange'].present?
       challenge_token = challenge_data['challenge']
@@ -22,7 +20,7 @@ class EventController < ApplicationController
     workspace = Workspace.find_by team_id: members_joined_data['team']
     channel_info = Slack::Api
       .channel_data(workspace.workspace_token,
-                    members_joined_data['channel_id']
+                    members_joined_data['channel']
                    )
     return workspace
       .channels
@@ -31,6 +29,8 @@ class EventController < ApplicationController
                         )
   end
 
+  private
+
   def challenge_data
     params.permit(:token, :challenge, :type)
   end
@@ -38,8 +38,5 @@ class EventController < ApplicationController
   def members_joined_data
     params.permit(:team, :channel)
   end
-
-  def channel_data
-    params.permit(:ok, channel: [ :id, :name ] )
-  end
 end
+
